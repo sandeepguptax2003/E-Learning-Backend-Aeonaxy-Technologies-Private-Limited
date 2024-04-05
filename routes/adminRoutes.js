c7b8f06d41cd655ca9383d84b2798e49346729ac
sandeep <sandeepguptax2003@gmail.com>
2024-04-05 20:06:32 +0530

    Updated adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware')
 
 // Admin login
 router.post('/login', adminController.loginAdmin);
